419a3e8f4a917a82bffe0601db9a40740355e797
Kunal Khadkeshwar <44850127+khadkeshwarkp@users.noreply.github.com>
2019-05-16 17:27:12 +0530

    Add files via upload

---
+++ costFunction.m
@@ -0,0 +1,10 @@
+function [J, grad] = costFunction(theta, X, y,lambda)
+m = length(y); J = 0;
+grad = zeros(size(theta));
+
+h_theta = sigmoid(X*theta);
+J = (1 / m) * ((-y' * log(h_theta)) - (1 - y)' * log(1 - h_theta));
+
+grad = (1 / m) * (h_theta - y)' * X;
+
+end
